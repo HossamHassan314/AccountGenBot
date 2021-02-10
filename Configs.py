f557d7372ef2acca908d2f5818e2c3cd8c6b4c78
HossamHassan314 <78844973+HossamHassan314@users.noreply.github.com>
2021-02-10 20:42:26 +0000

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -8,7 +8,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 6))
     ACCOUNT_GEN_NAME = os.environ.get("ACCOUNT_GEN_NAME", "Mail access")
     JTU_ENABLE = os.environ.get("JTU_ENABLE", True)
-    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", None)
+    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", O1_007)
     CHANNEL_URL = os.environ.get("CHANNEL_URL", "https://t.me/O1_007")
     DUMB_CHAT = int(os.environ.get("DUMB_CHAT", False))
     OWNER_ID = int(os.environ.get("OWNER_ID", None))
