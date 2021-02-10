861ef6bb987b6238a01c883879ee6969b00b898d
HossamHassan314 <78844973+HossamHassan314@users.noreply.github.com>
2021-02-10 19:35:28 +0000

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -9,7 +9,7 @@ class Config(object):
     ACCOUNT_GEN_NAME = os.environ.get("ACCOUNT_GEN_NAME", "Mail access")
     JTU_ENABLE = os.environ.get("JTU_ENABLE", False)
     CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", None)
-    CHANNEL_URL = os.environ.get("CHANNEL_URL", t.me/o1_07)
+    CHANNEL_URL = os.environ.get("CHANNEL_URL", "t.me/o1_07")
     DUMB_CHAT = int(os.environ.get("DUMB_CHAT", False))
     OWNER_ID = int(os.environ.get("OWNER_ID", None))
     GEN_LIMIT_PERDAY = int(os.environ.get("GEN_LIMIT_PERDAY", 2))
